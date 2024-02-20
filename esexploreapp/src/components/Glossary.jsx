b01790310ceb21c8a8581f656ecc14470a4be132
NicolaSavino <nick.savino@arcurve.com>
2024-02-19 18:24:23 -0700

    Added First Component

---
+++ esexploreapp/src/components/Glossary.jsx
@@ -0,0 +1,16 @@
+import { SafeAreaView } from "react-native";
+
+
+const Glossary = () => {
+
+    return (
+        <SafeAreaView>
+            <Text>Glossary</Text>
+            <View>
+                <Image source={require('../assets/placeholder.png')} />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Glossary;
