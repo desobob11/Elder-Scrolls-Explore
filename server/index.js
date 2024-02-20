d0a8656778ae185874de72bdb39f87d6b579022b
desobob11 <desmond.obrien@ucalgary.ca>
2024-02-19 19:36:45 -0700

    restructure server

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import { getFirestore, collection, query, where, getDocs, setDoc, doc, updateDoc, FieldValue, increment } from 'firebase/firestore';
+import { firebaseConfig, app, db } from "../../constants/firebase";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDemK_VxdFSkAuEegDJ5FnaJYq0sYszEfo",
+    authDomain: "esexplore-9fc9f.firebaseapp.com",
+    projectId: "esexplore-9fc9f",
+    storageBucket: "esexplore-9fc9f.appspot.com",
+    messagingSenderId: "643298327792",
+    appId: "1:643298327792:web:a05be6e84ce084923c1635"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//const express = require("express");
+//const app = express();
+//const port = 3000;
+
+
+
+
+/*
+app.get("/creatures", (req, res) => {
+
+})
+
+
+
+app.listen(port, () => {
+    console.log(`App is running on ${port}...`)
+})
+*/
---
+++ server/scrape.bat
@@ -0,0 +1,1 @@
+python createScraper.py
